e9d7df32612e04f853ca437ddbcacb006c8bb8c6
whd120130 <zlt120130@163.com>
2018-01-05 11:41:28 +0800

    HDI框架搭建

--- src/main/java/com/hdi/interceptor/ChanelInterceptor.java
+++ src/main/java/com/hdi/interceptor/ChanelInterceptor.java
@@ -1,16 +1,13 @@
 package com.hdi.interceptor;
 
-import com.alibaba.fastjson.JSONObject;
 import com.hdi.controller.BaseController;
 import com.hdi.model.commons.ResultStatus;
 import com.hdi.utils.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -25,30 +22,36 @@ public class ChanelInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         logger.info("****************preHandle");
-        String id = request.getParameter("id");
+        String userId = (String) request.getSession().getAttribute("id");
+        String requestURI = request.getRequestURI();
+        String ipAddress = request.getRemoteAddr();
+        String loginUrl = "/login.html";
         if (HandlerMethod.class.equals(handler.getClass())) {
             HandlerMethod method = (HandlerMethod) handler;
             Object controller = method.getBean();
             if (controller instanceof BaseController) {
-                String ipAddress = request.getParameter("ipAddress");
-                if (StringUtil.isNotEmpty(ipAddress)) {
-                    ((BaseController) method.getBean()).setIpAddress(ipAddress);
-                }
-                if (StringUtil.isEmpty(id) || "0".equals(id)) {
-                    logger.error("系统无法获取当前登录用户");
-                    JSONObject jsonObj = new JSONObject();
-                    jsonObj.put("status", ResultStatus.LOGIN_ERROE.getCode());
-                    jsonObj.put("message", "系统无法获取当前登录用户");
-                    jsonObj.put("value", null);
-                    response.setCharacterEncoding("UTF-8");
-                    response.setContentType("application/json;charset=UTF-8");
-                    response.getWriter().write(jsonObj.toString());
-                    return false;
-                } else {
-                    ((BaseController) method.getBean()).setId(Integer.parseInt(id));
-                    logger.info(Thread.currentThread().getName() + " set userId:" + id);
+                String uri = requestURI.substring(requestURI.lastIndexOf("/"));
+                if (uri.startsWith("/login") || uri.startsWith("/imageLogin") || uri.startsWith("/checkLogin")){
+                    ((BaseController) method.getBean()).setId(Integer.parseInt(userId));
                     return true;
+                }else {
+                    // 非法请求 重定向到登录页面
+                    response.sendRedirect(request.getContextPath() + loginUrl);
+                    return false;
                 }
+//                if (StringUtil.isEmpty(userId)) {
+////                    logger.error(ResultStatus.LOGIN_ERROE.getMessage());
+////                    ResultBean resultBean = ResultBean.buildError(ResultStatus.LOGIN_ERROE.getCode(),ResultStatus.LOGIN_ERROE.getMessage());
+////                    response.setCharacterEncoding("UTF-8");
+////                    response.setContentType("application/json;charset=UTF-8");
+////                    response.getWriter().write(JSONObject.toJSONString(resultBean));
+//
+//                    return false;
+//                } else {
+//                    ((BaseController) method.getBean()).setId(Integer.parseInt(userId));
+//                    logger.info(Thread.currentThread().getName() + " set userId:" + userId);
+//                    return true;
+//                }
             }
         }
         return false;
--- src/main/java/com/hdi/model/commons/ResultStatus.java
+++ src/main/java/com/hdi/model/commons/ResultStatus.java
@@ -10,7 +10,7 @@ public enum ResultStatus {
     SUCCESS(0, "成功"),
     FAILD(-1, "失败"),
     EXCEPTION(-1, "系统异常"),
-    LOGIN_ERROE(-4,"登录异常"),
+    LOGIN_ERROE(-4,"系统无法获取当前登录用户"),
     PARAM_ERROR(1000, "参数错误"),
     SYSTEM_ERROR(1001, "系统错误"),
     PUTPEOPLE_RIGHT_ERROE(1001, "右安置人已经存在，不能放在该人的下面"),
